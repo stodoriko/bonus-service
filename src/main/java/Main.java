b49ae8d44a6a6d55c771b341fbe914f370c068ca
stodoriko <s.todorikos@gmail.com>
2021-07-31 21:57:18 +0300

    fixed bonus-service [2]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,10 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    long bonus = service.calculate(amount, registered);
+    service.calculate(amount, registered);
 
-    System.out.println(bonus);
+//    long bonus = service.calculate(amount, registered);
+//
+//    System.out.println(bonus);
   }
 }
